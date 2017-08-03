b3881b9312710cb5a99d952567ea9a73791fa2a0
adan19-meet <adan19@meet.mit.edu>
2017-08-03 11:59:26 +0300

    luna did this

--- app.py
+++ app.py
@@ -14,31 +14,31 @@ session = DBSession()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/sign_up')
 def sign_up():
-    return render_template('sign_up.html')
+	return render_template('sign_up.html')
 
 @app.route('/categories')
 def categories():
-    return render_template('categories.html')
+	return render_template('categories.html')
 
 @app.route('/categories/<string:cat>')
 def selected_category(cat):
 	videos = session.query(Video).filter_by(category=cat).all()
-    return render_template('category.html', videos = videos, cat = cat)
+	return render_template('category.html', videos = videos, cat = cat)
 
 @app.route('/favorites')
 def favorites():
-    return render_template('favorites.html')
+	return render_template('favorites.html')
 
 @app.route('/sign_in')
 def sign_in():
-    return render_template('sign_in.html')
+	return render_template('sign_in.html')
 
-@app.route('/profile')
-def profile(id):
-   # profile = session.query(User).filter_by(id=id).first()
-    return render_template('profile.html', profile=profile )
+@app.route('/profile/<int:profile_id>')
+def profile(profile_id):
+	profile = session.query(User).filter_by(id=profile_id).first()
+	return render_template('profile.html', profile = profile)
 
